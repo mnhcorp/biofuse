ad3b1e64c6a8b67da807ef4ab151792c2faf6cdb
Mirza Hossain <mnhcorp@gmail.com>
2025-07-06 17:59:57 +0100

    Transform image for BUSI

--- biofuse/models/data_adapter.py
+++ biofuse/models/data_adapter.py
@@ -76,13 +76,18 @@ class DataAdapter:
         else: # split == 'test'
             split_images, split_labels = test_images, test_labels
             
+        transform = transforms.Compose([
+            transforms.Resize((img_size, img_size)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
         dataset = BioFuseImageDataset(
             images=split_images,
             labels=split_labels,
             path=True,
             rgb=True, # BUSI images are grayscale but we'll convert to RGB
-            resize=True,
-            img_size=img_size
+            transform=transform
         )
         
         return dataset, 2
