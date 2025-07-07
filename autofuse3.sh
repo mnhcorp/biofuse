409ab2c05035ed60020123e00ac5500b10dd041c
Mirza Hossain <mnhcorp@gmail.com>
2025-07-07 16:56:54 +0100

    Add new autofuse3.sh

--- autofuse3.sh
+++ autofuse3.sh
@@ -8,13 +8,13 @@ epochs=1
 
 # Model Legend: BC: BioMedCLIP, PC: PubMedCLIP, CO: CONCH, RD: rad-dino, UN: UNI, PG: Prov-GigaPath, HB: Hibou-B, CA: CheXagent, CL: CLIP
 # replace the code with model names from 'models' variable
-models=$(echo $models | sed 's/BC/BioMedCLIP/g' | sed 's/PC/PubMedCLIP/g' | sed 's/CO/CONCH/g' | sed 's/RD/rad-dino/g' | sed 's/UN/UNI/g' | sed 's/PG/Prov-GigaPath/g' | sed 's/HB/Hibou-B/g' | sed 's/CA/CheXagent/g' | sed 's/CL/CLIP/g')
+models=$(echo $models | sed 's/BC/BioMedCLIP/g' | sed 's/PC/PubMedCLIP/g' | sed 's/CO/CONCH/g' | sed 's/RD/rad-dino/g' | sed 's/UN/UNI/g' | sed 's/UN2/UNI2/g' |sed 's/PG/Prov-GigaPath/g' | sed 's/HB/Hibou-B/g' | sed 's/CA/CheXagent/g')
 
-datasets=("breastmnist" "dermamnist" "octmnist" "pathmnist" "pneumoniamnist" "retinamnist" "organamnist" "organsmnist" "organcmnist" "bloodmnist" "chestmnist" "tissuemnist")
+datasets=("breastmnist") # "octmnist" "pathmnist" "pneumoniamnist" "retinamnist" "organamnist" "organsmnist" "organcmnist" "bloodmnist" "tissuemnist" "chestmnist")
 
 for dataset in ${datasets[@]}; do
     echo "python tests/test_linear_probe_trainable2.py --dataset $dataset --img_size $size --models $models --fusion_methods $fusion_methods --num_epochs=$epochs"
-    python tests/test_linear_probe_trainable2.py --dataset $dataset --img_size $size --models $models --fusion_methods $fusion_methods --num_epochs=$epochs --single #--projections 512,1024,2048
+    python tests/test_linear_probe_trainable2.py --dataset $dataset --img_size $size --models $models --fusion_methods $fusion_methods --num_epochs=$epochs #--single #--projections 512,1024,2048
 done
 
 # echo "python tests/test_linear_probe_trainable2.py --dataset $dataset --img_size $size --models $models --fusion_methods $fusion_methods --num_epochs=$epochs --single"
